88aae713626d40dfc6f721116c5d5970f017f448
Leonardo Alves <leoves.oliveira@gmail.com>
2019-01-22 17:29:25 -0200

    add jump to links

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,16 @@ window.addEventListener('scroll', function(){
     header.classList.remove("header--sticky");
   }
 });
+
+document.querySelector('body').addEventListener('click', function (e) {
+  if (e.target.classList.contains('jump-to')) {
+      e.preventDefault();
+
+      Jump(
+          e.target.getAttribute("href"),
+          {
+              offset: -55
+          }
+      );
+  }
+});
